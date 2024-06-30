074084dde5edd520a6fee43a4565420c7bdbe427
Aditya <helloadig@gmail.com>
2024-06-30 12:39:22 +0530

    Minor changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import { Box, Stack, Typography } from '@mui/material';
 
 const Footer = () => (
   <Box m="80px 0" >
-    <Typography variant="h5" sx={{ fontSize: { lg: '28px', xs: '20px' } }} mt="41px" textAlign="center" >Made by <a href="https://github.com/aaadityaG" style={{textDecoration: 'none', color: 'yellow'}} target="_blank">Aditya</a> </Typography>
+    <Typography variant="h5" sx={{ fontSize: { lg: '28px', xs: '20px' } }} mt="41px" textAlign="center" >Made by <a href="https://github.com/rushikesh-koli-11" style={{textDecoration: 'none', color: 'yellow'}} target="_blank">Rushikesh</a> </Typography>
   </Box>
 );
 
